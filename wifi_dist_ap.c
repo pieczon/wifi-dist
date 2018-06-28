de6f332d60782e7da8fffba331ef6bd3f0ebceba
pieczon <31313385+pieczon@users.noreply.github.com>
2018-06-28 02:28:01 +0200

    Add files via upload

--- wifi_dist_ap.c
+++ wifi_dist_ap.c
@@ -8,6 +8,8 @@
 #include <termios.h>
 #include <sys/time.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -19,16 +21,16 @@
 
 struct wifi_scan *wifi = NULL;
 struct station_info station;    
-char mac[BSSID_STRING_LENGTH], key;
-int conn_stat, tlumienie_1m;
-double indeks_gamma, zanik_mocy, tlumienie_swobod_przestrz;
+char mac[BSSID_STRING_LENGTH], key, inputs[10];
+int conn_stat, sg, st, liczba, tlumienie_1m;
+float indeks_gamma, zanik_mocy, tlumienie_swobod_przestrz;
 
 static struct termios g_old_kbd_mode;
 
 sem_t watek1, watek2;
 pid_t pid;
 
-static int keypressed(void) //did somebody press somthing??? 
+static int keyPressed(void) //did somebody press somthing??? 
 { 
     struct timeval timeout; 
     fd_set read_handles; 
@@ -60,6 +62,21 @@ void Usage(char **argv)
 	printf("%s wlan0\n", argv[0]);	
 }
 
+bool isNumber(char *string)
+{
+	printf("co odebrano<- %s\n", string);
+	int n = (sizeof(string)/sizeof(int)); //sizeof(string[0]) to nie dziala liczy 1 el. wiecej dla samych intów
+    for (int i=0; i<n; i++)
+	{
+		printf("%d el. stringa: %c\n", i, string[i]);
+		if(isdigit(string[i]) == false)
+		{
+			return false;
+		}
+	}
+    return true;
+}
+
 float mod_OneSlope(int sta_signal, int supp_1m, double absorption, int dbi_gain)
 {
 	float pomiar = pow(10,(double)((sta_signal - dbi_gain) + supp_1m)/absorption);
@@ -87,33 +104,32 @@ float mod_MultiWall(int sta_signal, int dbi_gain, int supp_1m, double absorption
 void *pomiary(void *arg)
 {
 	for(;;)
-		{	
-			sem_wait(&watek1);
-			//printf("\n-----jestem 1 wątek!-----\n");
+	{	
+		sem_wait(&watek1);
 
-			conn_stat = wifi_scan_station(wifi, &station);
-			if(conn_stat == 0)
-			{
-				printf("Brak połączenia Wi-Fi\n");
-			}
-			else if(conn_stat == -1)
-			{
-				perror("Nie można uzyskać informacji o połączeniu\n");
-			}
-			else
-			{
-				printf("%s "ANSI_COLOR_GREEN"%s"ANSI_COLOR_RESET" signal "ANSI_COLOR_YELLOW"%d"ANSI_COLOR_RESET"dBm signal_avg "ANSI_COLOR_YELLOW"%d"ANSI_COLOR_RESET"dBm\n One-Slope "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET" Model-liniowy "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET" Swobodnej-przestrzeni "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET" Multi-Wall "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET"\n\n", bssid_to_string(station.bssid,mac), 
-				station.ssid, 
-				station.signal_dbm, 
-				station.signal_bea, 
-				mod_OneSlope(station.signal_dbm, tlumienie_1m, zanik_mocy, 20),
-				mod_ModelLiniowy(station.signal_dbm, tlumienie_swobod_przestrz, 20), 
-				mod_SwobodnejPrzestrzeni(station.signal_dbm, 20),
-				mod_MultiWall(station.signal_dbm, 20, tlumienie_1m, tlumienie_swobod_przestrz));
-			}
-			sleep(1);
-			sem_post(&watek2);
+		conn_stat = wifi_scan_station(wifi, &station);
+		if(conn_stat == 0)
+		{
+			printf("Brak połączenia Wi-Fi\n");
 		}
+		else if(conn_stat == -1)
+		{
+			perror("Nie można uzyskać informacji o połączeniu\n");
+		}
+		else
+		{
+			printf("%s "ANSI_COLOR_GREEN"%s"ANSI_COLOR_RESET" signal "ANSI_COLOR_YELLOW"%d"ANSI_COLOR_RESET"dBm signal_avg "ANSI_COLOR_YELLOW"%d"ANSI_COLOR_RESET"dBm\n One-Slope "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET" Model-liniowy "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET" Swobodnej-przestrzeni "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET" Multi-Wall "ANSI_COLOR_RED"%.2f"ANSI_COLOR_RESET"\n\n", bssid_to_string(station.bssid,mac), 
+			station.ssid, 
+			station.signal_dbm, 
+			station.signal_bea, 
+			mod_OneSlope(station.signal_dbm, tlumienie_1m, zanik_mocy, 20),
+			mod_ModelLiniowy(station.signal_dbm, tlumienie_swobod_przestrz, 20), 
+			mod_SwobodnejPrzestrzeni(station.signal_dbm, 20),
+			mod_MultiWall(station.signal_dbm, 20, tlumienie_1m, tlumienie_swobod_przestrz));
+		}
+		sleep(1);
+		sem_post(&watek2);
+	}
 }
 
 void *koniec(void *arg)
@@ -121,13 +137,10 @@ void *koniec(void *arg)
     for(;;)
     {
         sem_wait(&watek2);
-		//printf("\n-----jestem 2 wątek!-----\n");
-
-		//printf("Press 'q' for exit \n"); 
-		if(keypressed())
+		if(keyPressed())
 		{ 
-			key = getchar(); //getting the pressed key... 
-			printf("Wciśnięto klawisz - %c\n", key); 
+			key = getchar();
+
 			if(key == 27)
 			{ 
 				printf("Naciśnięto ESC\n");
@@ -135,39 +148,47 @@ void *koniec(void *arg)
 				exit(1); 
 			} 
 		}
-
-		// pid = fork();
-		// if(pid == -1)
-		// {
-		// 	perror("fork error\n");	
-		// }
-		// if(pid == 0)
-		// {
-		// 	printf("child pid = %d\n",getpid());
-		// 	printf("child parent pid = %d\n", getppid());
-			
-		// }
-		// else if(pid > 0)
-		// {
-		// 	printf("parent pid = %d\n",getpid());
-		// 	printf("parent parent pid = %d\n",getppid());
-		// }
 		sem_post(&watek1);
     }
 }
 
 int main(int argc, char **argv)
 {
-	printf(ANSI_COLOR_YELLOW"\nProgram oblicza szacunkową odległość połączonego urządzenia w sieci Wi-Fi od Routera/AP, w metrach [m] z dokładnością do 2 miejsc po przecinku\n"ANSI_COLOR_RESET);
-	printf(ANSI_COLOR_YELLOW"Zakończenie pomiarów = Q\n\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_YELLOW"\nProgram oblicza szacunkową odległość połączonego urządzenia w sieci Wi-Fi od Routera/AP.\nPomiar prezentowany jest w metrach "ANSI_COLOR_RED"[m]"ANSI_COLOR_YELLOW", z dokładnością do 2 miejsc po przecinku.\n\n"ANSI_COLOR_RESET);
 
-	printf(ANSI_COLOR_GREEN"Podaj wartość tlumienia w odległości 1 [m]: "ANSI_COLOR_RESET);
-	scanf("%d", &tlumienie_1m); //zabezpieczyc przed wpisaniem dowolnych znakow
-	printf(ANSI_COLOR_GREEN"Podaj wartość indeksu gamma: "ANSI_COLOR_RESET);
-	scanf("%lf", &indeks_gamma); //zabezpieczyc przed wpisaniem dowolnych znakow
-	printf("\n\n");
+	printf(ANSI_COLOR_GREEN"Podaj szacowaną wartość tłumienia w odległości 1"ANSI_COLOR_RED"[m]"ANSI_COLOR_GREEN" od Routera/AP [1 - 99]: "ANSI_COLOR_RESET);
 
-	zanik_mocy = indeks_gamma*10;
+	st = scanf("%s", inputs);
+	printf("co wpisano-> %s\n", inputs);
+
+	while(isNumber(inputs) == false)
+	{	
+		printf(ANSI_COLOR_RED"Nie podano liczby, wpisz ponownie: "ANSI_COLOR_RESET);
+		st = scanf("%s", inputs);
+		printf("co wpisano-> %s\n", inputs);
+		printf("+co daje isNumber: %d\n\n", isNumber(inputs));
+	}
+	printf("co mamy w inputs: %s\n", inputs);
+	tlumienie_1m = *inputs; //to rzutowanie nie działa poprawnie
+
+	printf("jakie mamy tlumienie: %d\n", tlumienie_1m);
+	while((tlumienie_1m < 1) || (tlumienie_1m > 99))
+	{
+		printf(ANSI_COLOR_RED"Błędna liczba, podaj ponownie: "ANSI_COLOR_RESET);
+		st = scanf("%d", &tlumienie_1m);
+	}
+
+	printf(ANSI_COLOR_GREEN"Podaj wartość indeksu gamma [3.5 - 6]: "ANSI_COLOR_RESET);
+	sg = scanf("%f", &indeks_gamma);
+	while((indeks_gamma < 3.5) || (indeks_gamma > 6))
+	{
+		printf(ANSI_COLOR_RED"Błędna liczba, podaj ponownie: "ANSI_COLOR_RESET);
+		sg = scanf("%f", &indeks_gamma);
+	}
+
+	printf(ANSI_COLOR_CYAN"\nRozpoczynam obliczenia...(zakończenie pomiarów klawisz ESC)\n\n"ANSI_COLOR_RESET);
+
+	zanik_mocy = (-1)*indeks_gamma*10;
 	tlumienie_swobod_przestrz = (-27.55+20*log10(2437)+20*log10(9)); //to przejrzec, sprawdzic co znaczy kazda liczba jeszcze raz, przekalkulowac nowa zmienna dbi_gain
 
 	if(argc != 2)
